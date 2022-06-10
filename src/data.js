5433f7c0fca9f43207e97e3cf71e41d44f5aa261
ArbabHaiderCheema <77755491+ArbabHaiderCheema@users.noreply.github.com>
2022-06-11 02:23:56 +0500

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,61 @@
+// src/data.js
+
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project.jpg",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project.jpg",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project.jpg",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project.jpg",
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "React",
+  "PHP",
+];
+
+
+export const testimonials  = [
+  {
+    quote:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./customer-icon.png",
+    name:"Lorem ipsum dolor",
+    company: "Company",
+  },
+  {
+    quote:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./customer-icon.png",
+    name:"Lorem ipsum dolor",
+    company: "Company",
+  },
+];
